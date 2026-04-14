30806333fe779ed544518d17e82a8800b7fd857e
Shravanth <shravanth.k.2007@gmail.com>
2026-04-14 21:16:09 +0530

    change q4.c termination

--- q4/q4.c
+++ q4/q4.c
@@ -4,13 +4,13 @@
 // Allows to load/unload shared libraries while program is running
 
 int main(){
-    while(1){
+
+    char op[6];
+    int num1, num2;
+
+    while(scanf("%s %d %d", op, &num1, &num2) == 3){
         char s1[14] = "./lib";
         char s2[] =  ".so";
-        char op[6];
-        int num1, num2;
-
-        scanf("%s %d %d", op, &num1, &num2);
 
         strcat(s1, op);
         strcat(s1, s2);
@@ -19,20 +19,8 @@ int main(){
         void* handle = dlopen(s1, RTLD_LAZY);
         // Allows us to resolve only the <op> function when required
 
-        if(!handle){
-            printf("Could not find the library: %s\n", dlerror());
-            continue;
-        }
-
         int (*operation)(int, int) = (int (*) (int, int)) dlsym(handle, op);
         // Finds the memory address of <op> function
-
-        char* error = dlerror();
-        if(error != NULL){
-            printf("Could not find the operation: %s\n", error);
-            dlclose(handle);
-            continue;
-        }
 
         int ans = operation(num1, num2);
         printf("%d\n", ans);
